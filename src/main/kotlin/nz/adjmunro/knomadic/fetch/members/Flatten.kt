dfaa23ef7a6821af4590e9505c3eee07fe457f7e
Adam Munro <adam.munro@orbitremit.com>
2025-09-21 22:14:02 +1200

    refactor: rename & extract fetch children

--- src/main/kotlin/nz/adjmunro/knomadic/fetch/members/Flatten.kt
+++ src/main/kotlin/nz/adjmunro/knomadic/fetch/members/Flatten.kt
@@ -1,11 +1,18 @@
 package nz.adjmunro.knomadic.fetch.members
 
-import nz.adjmunro.knomadic.KnomadicDsl
+import nz.adjmunro.inline.itself
 import nz.adjmunro.knomadic.fetch.Fetch
+import nz.adjmunro.knomadic.fetch.FetchDsl
+import nz.adjmunro.knomadic.fetch.Fetching
+import nz.adjmunro.knomadic.fetch.Finished
 
 /**
  * Flatten a nested [Fetch] into a single [Fetch].
  */
-@KnomadicDsl
+@FetchDsl
 public inline val <T : Any> Fetch<Fetch<T>>.flatten: Fetch<T>
-    get() = flatMapFinished { it }
+    get() = fold(
+        prefetch = ::itself,
+        fetching = { cache ?: Fetching() },
+        finished = Finished<Fetch<T>>::result,
+    )
